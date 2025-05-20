1135129f2b378fa60b7db54bff8c67ac80dbc9c6
Ardkrav <ardkrav@gmail.com>
2025-05-20 12:36:30 -0300

    feat (Springboot): Inicializar nuevo proyecto 'chess'

---
+++ src/test/java/com/springboot/chess/TestChessApplication.java
@@ -0,0 +1,11 @@
+package com.springboot.chess;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ChessApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
